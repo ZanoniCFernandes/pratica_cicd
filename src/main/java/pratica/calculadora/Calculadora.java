9b607ba8eb068ef980bf4ae6db20fd1ea5a55796
Zanoni Campos Fernandes - 12723131956 <12723131956@ulife.com.br>
2024-06-03 21:24:43 -0300

    Configurando GitHub Actions

---
+++ src/main/java/pratica/calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package pratica.calculadora;
+
+public class Calculadora {
+
+    public float soma(float x, float y) {
+      return x + y;
+    }
+  
+    public float subtrai(float x, float y) {
+      return x - y;
+    }
+  }
